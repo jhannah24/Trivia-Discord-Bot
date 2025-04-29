4b27e7291f25ac4c1a071fb7e84682d3312441e0
jhannah24 <113557874+jhannah24@users.noreply.github.com>
2025-04-28 22:01:11 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 import { Client, Events, GatewayIntentBits } from 'discord.js';
 import { config } from 'dotenv';
-import * as test from './commands/test.js';
 import * as trivia from './commands/trivia.js';
 import * as kaicenat from './commands/kaicenat.js';
 
@@ -23,9 +22,7 @@ client.login(process.env.TOKEN);
 async function handleInteraction(interaction) {
     if (!interaction.isCommand()) return;
 
-    if (interaction.commandName === 'test') {
-        await test.execute(interaction);
-    } else if (interaction.commandName === 'trivia') {
+    if (interaction.commandName === 'trivia') {
         await trivia.execute(interaction);
     } else if (interaction.commandName === 'kaicenat') {
         await kaicenat.execute(interaction);
